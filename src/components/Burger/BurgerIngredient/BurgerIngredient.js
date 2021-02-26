ae8628a559c5bdccfced4d3efac8363f1dd40edb
twolak88 <twolak88@gmail.com>
2021-02-26 20:23:17 +0100

    Adding Prop Type Validation for BurgerIngredient

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,27 +1,37 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import classes from './BurgerIngredient.module.css';
 
-const BurgerIngredient = (props) => {
-  switch (props.type) {
-    case('bread-bottom'):
-      return <div className={classes.BreadBottom}></div>;
-    case('bread-top'):
-      return (
-        <div className={classes.BreadTop}>
-          <div className={classes.Seeds1}></div>
-          <div className={classes.Seeds2}></div>
-        </div>
-      );
-    case('meat'):
-      return <div className={classes.Meat}></div>;
-    case('cheese'):
-      return <div className={classes.Cheese}></div>;
-    case('salad'):
-      return <div className={classes.Salad}></div>;
-    case('bacon'):
-      return <div className={classes.Bacon}></div>;
-    default:
-      return null;
+class BurgerIngredient extends Component {
+
+  render() {
+    switch (this.props.type) {
+      case 'bread-bottom':
+        return <div className={classes.BreadBottom}></div>;
+      case 'bread-top':
+        return (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>
+        );
+      case 'meat':
+        return <div className={classes.Meat}></div>;
+      case 'cheese':
+        return <div className={classes.Cheese}></div>;
+      case 'salad':
+        return <div className={classes.Salad}></div>;
+      case 'bacon':
+        return <div className={classes.Bacon}></div>;
+      default:
+        return null;
+    }
   }
 };
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+}
 
 export default BurgerIngredient;
