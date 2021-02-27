b141d2a35cdf95865a75c6e51e162a0ba7161242
twolak88 <twolak88@gmail.com>
2021-02-27 15:18:27 +0100

    More about Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,9 @@ const Toolbar = (props) => (
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <NavigationItems />
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
