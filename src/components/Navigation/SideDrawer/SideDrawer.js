2a50ed2160a1e4c7bd0573c2a7dd127b249cefdf
twolak88 <twolak88@gmail.com>
2021-02-27 15:50:29 +0100

    Reusing the Backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,30 @@
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const SideDrawer = (props) => {
-  //...
+  let attaachedClasses = [classes.SideDrawer];
+  if (props.opened) {
+    attaachedClasses.push(classes.Open);
+  } else {
+    attaachedClasses.push(classes.Close);
+  }
   return(
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop 
+        show={props.opened} 
+        clicked={props.closed}/>
+      <div className={attaachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav> 
       </div>
-      <nav>
-        <NavigationItems />
-      </nav> 
-    </div>
+    </Aux>
   );
 };
 
