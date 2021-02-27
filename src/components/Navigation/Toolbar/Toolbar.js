82ed9af69761a16c5fdaf3afae15952d82dc53c8
twolak88 <twolak88@gmail.com>
2021-02-27 14:46:49 +0100

    Working on Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,9 @@ import classes from './Toolbar.module.css';
 const Toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
     <NavigationItems />
   </header>
 );
