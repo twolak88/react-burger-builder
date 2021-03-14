ef4b93f41fd5afc2e33182f74f5c982c9c698537
twolak88 <twolak88@gmail.com>
2021-03-14 11:01:07 +0100

    Adding a Dropdown Component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,23 +4,42 @@ const Input = (props) => {
   let inputElement = null;
 
   switch (props.elementType) {
-    case ('input'):
-      inputElement = <input 
-        className={classes.InputElement} 
-        {...props.elementConfig} 
-        value={props.value} />
+    case 'input':
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value} />
+      );
       break;
-    case ('textarea'):
-      inputElement = <textarea 
-        className={classes.InputElement} 
-        {...props.elementConfig} 
-        value={props.value} />
+    case 'textarea':
+      inputElement = (
+        <textarea
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value} />
+      );
+      break;
+    case 'select':
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          value={props.value} >
+          {props.elementConfig.options.map(option => (
+            <option 
+              key={option.value}
+              value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>
+      );
       break;
     default:
-      inputElement = <input 
-        className={classes.InputElement} 
-        {...props.elementConfig} 
-        value={props.value} />
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value} />
+      );
       break;
   }
   return (
@@ -29,6 +48,6 @@ const Input = (props) => {
       {inputElement}
     </div>
   );
-}
+};
 
 export default Input;
